0d522ef3bd82df42939e097ae2a870cadf0599d2
jggomezt <juan.gomez01@gmail.com>
2018-09-19 21:36:27 -0500

    se desarrolla los layouts

---
+++ app/src/main/java/co/devhack/homecommunity/ClaimsActivity.kt
@@ -0,0 +1,12 @@
+package co.devhack.homecommunity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ClaimsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_claims)
+    }
+}
